e3cb0759525c5c087579e883fb7647cb264f5f92
Max Kirchoff <max@lamasix.com>
2019-02-26 18:12:38 -0500

    new site

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,84 +1,101 @@
 /*!
- * lamasix-landing-page
- * Lama Six Landing page 2019
+ * lamasix.com
+ * Website for the creative agency, LAMA SIX
  * https://github.com/LamaSix/lamasix.com
  * @author Max Kirchoff
- * @version 1.9.0
+ * @version 1.0.0
  * Copyright 2019. MIT licensed.
  */
 (function ($, window, document, undefined) {
 
   'use strict';
 
-  function scrollText() {
-    killScrollText();
-    $('.cloned').remove();
-
-    var $holder = $(".holder");
-    var $list = $holder.find("ul.list");
-    var $clonedList = $list.clone();
-
-    var listWidth = $(window).width();
-    var endPos = $holder.width() - listWidth;
-
-    $list.add($clonedList).css({
-    	"width" : listWidth + "px"
+  $(function () {
+    $('button.menu-toggle').on('click touch', function(ev) {
+      ev.preventDefault();
+      $('ul.nav-items').toggleClass('active');
+      $('ul.social-items').toggleClass('active');
+      $(this).parents('.header').toggleClass('menu-active');
+      $('body').toggleClass('menu-active');
     });
 
-    $clonedList.addClass("cloned").appendTo($holder);
+    function updateHeader() {
+      var $header = $('header.header');
+      var headerPos = $header.offset().top;
 
-    //TimelineMax
-    var infinite = new TimelineMax({repeat: -1, paused: false});
-    var time = 30;
-
-    infinite.fromTo($list, time, {left:0}, {left: -listWidth, ease: Linear.easeNone}, 0);
-    infinite.fromTo($clonedList, time, {left:listWidth}, {left:0, ease: Linear.easeNone}, 0);
-    infinite.set($list, {left: listWidth});
-    infinite.to($clonedList, time, {left: -listWidth, ease: Linear.easeNone}, time);
-    infinite.to($list, time, {left: 0, ease: Linear.easeNone}, time);
-
-    //Pause/Play
-
-    $holder.on("mouseenter", function(){
-    	infinite.pause();
-    }).on("mouseleave", function(){
-    	infinite.play();
-    });
-  }
-
-  function killScrollText() {
-    TweenMax.killAll(false, true, false);
-  }
-
-  $(window).on('resize', function() {
-    if ($(window).width() > 768) {
-      killScrollText();
-      scrollText();
-    } else {
-      killScrollText();
+      if (headerPos > 0) {
+        $header.addClass('scrolled');
+      } else {
+        $header.removeClass('scrolled');
+      }
     }
+
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+    // on scroll, let the interval function know the user has scrolled
+    $(window).scroll(function(event) {
+      didScroll = true;
+    });
+
+    // run hasScrolled() and reset didScroll status
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 50);
+
+    function hasScrolled() {
+      updateHeader();
+      var st = $(window).scrollTop();
+
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+          // Scroll Down
+          $('header.header').removeClass('nav-down').addClass('nav-up');
+      } else {
+          // Scroll Up
+          if(st + $(window).height() < $(document).height()) {
+              $('header.header').removeClass('nav-up').addClass('nav-down');
+          }
+      }
+
+      lastScrollTop = st;
+    }
+
+    updateHeader();
+
+    $('.quotes').slick({
+      arrows: true,
+      prevArrow: $('.slick-navigation .prev'),
+      nextArrow: $('.slick-navigation .next'),
+      centerPadding: '0px',
+      centerMode: true,
+      slidesToShow: 1,
+      dots: false,
+      responsive: [
+        {
+          breakpoint: 800,
+          settings: {
+            dots: true,
+            arrows: false
+          }
+        }
+      ]
+    });
+
+    $('#home a.work').on('click touch', function(ev) {
+      ev.preventDefault();
+      var workPos = $('#work').offset().top;
+      window.scrollTo({ top: workPos, behavior: 'smooth' });
+    });
   });
-  if ($(window).width() > 768) {
-    scrollText();
-  }
-
-  var vid = document.getElementById("l6-animation-video");
-
-  function playVid() {
-      vid.play();
-  }
-
-  function pauseVid() {
-      vid.pause();
-  }
-
-setTimeout( function() {
-  playVid();
-  setInterval(function() {
-    playVid();
-  }, 7000);
-}, 1000);
-
-
 
 })(jQuery, window, document);
